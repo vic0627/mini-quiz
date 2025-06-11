e77a2803d52b652f448d94d97c3b0b59252425d1
victor_xu <victor_xu@auer.com.tw>
2025-06-11 16:55:37 +0800

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import "./style.css";
-import 'element-plus/dist/index.css'
+import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
 import App from "./App.vue";
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(ElementPlus);
+app.use(pinia);
 app.mount("#app");
